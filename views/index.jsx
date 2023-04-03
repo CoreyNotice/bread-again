bcf73ad3a5abcac03ab28edfe78197baf03ad8e3
Corey N <coreynotice@yahoo.com>
2023-04-03 12:55:33 -0400

    index page rendered+readme

---
+++ views/index.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Default = require('./layout/default')
+
+function index () {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+      </Default>
+    )   
+}
+
+module.exports =index
